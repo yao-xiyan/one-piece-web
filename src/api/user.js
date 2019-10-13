6973624e48b77b1ede300f3a3ccf910eff14223c
yao-xiyan <769695241@qq.com>
2019-10-13 10:58:43 +0800

    封装axios的API请求

---
+++ src/api/user.js
@@ -0,0 +1,20 @@
+/**
+ * 用户相关接口封装模块
+ * 最佳实践：建议将所有请求都封装成一个一个的小函数，在需要的时候直接调用
+ *   好处：1. 好维护，统一管理 2. 可重用
+ * 遵循一个原则：不要直接在组件中发请求，都封装成函数进行调用
+ */
+import request from '@/utils/request'
+
+export const login = (data) => {
+  // 其它逻辑。。。
+  return request({
+    method: 'POST',
+    url: '/app/v1_0/authorizations',
+    data
+  })
+}
+
+export function register () {
+
+}
--- src/views/index.vue
+++ src/views/index.vue
@@ -32,7 +32,8 @@
 </template>
 
 <script>
-import request from '@/utils/request'
+// import request from '@/utils/request'
+import { login } from '@/api/user'
 export default {
   name: 'LoginIndex',
   data () {
@@ -46,17 +47,26 @@ export default {
   methods: {
     // 登录按钮事件
     async onLogin () {
+      const toast = this.$toast.loading({
+        duration: 0, // 持续展示 toast
+        forbidClick: true, // 禁用背景点击
+        loadingType: 'spinner',
+        message: '登录中···'
+      })
       try {
+        // 表单验证
+
+        // 验证通过，loding，请求
         // 请求提交表单数据
-        const { data } = await request({
-          method: 'POST',
-          url: '/app/v1_0/authorizations',
-          data: this.user
-        })
+        const { data } = await login(this.user)
         console.log(data)
+        // 结束loading
+        toast.clear()
         this.$toast.success('登陆成功')
       } catch (err) {
         if (err.response && err.response.status === 400) {
+          // 结束loading
+          toast.clear()
           this.$toast.fail('登录失败，手机号或验证码错误！')
         }
       }
