fe6dcf25604fb2b881c76e674827f0f5e5b5ddc0
yao-xiyan <769695241@qq.com>
2019-10-25 01:39:15 +0800

    评论回复 - 展示评论回复列表

--- src/views/article/components/comment-reply.vue
+++ src/views/article/components/comment-reply.vue
@@ -1,62 +1,77 @@
 <template>
   <div class="article-comments">
-    <!-- 评论列表 -->
+    <!-- 导航栏 -->
+    <van-nav-bar :title="comment.reply_count + ' 条回复'">
+      <van-icon slot="left"
+                name="cross" />
+    </van-nav-bar>
+    <!-- /导航栏 -->
+
+    <!-- 当前评论 -->
+    <van-cell title="当前评论">
+      <van-image slot="icon"
+                 round
+                 width="30"
+                 height="30"
+                 style="margin-right: 10px;"
+                 :src="comment.aut_photo" />
+      <span style="color: #466b9d;"
+            slot="title">{{ comment.aut_name }}</span>
+      <div slot="label">
+        <p style="color: #363636;">{{ comment.content }}</p>
+        <p>
+          <span style="margin-right: 10px;">{{ comment.pudate | relativeTime }}</span>
+          <van-button size="mini"
+                      type="default">回复 {{ comment.reply_count }}</van-button>
+        </p>
+      </div>
+      <van-icon slot="right-icon" />
+    </van-cell>
+    <!-- /当前评论 -->
+
+    <van-cell title="全部评论" />
+
+    <!-- 回复列表 -->
     <van-list v-model="loading"
               :finished="finished"
               finished-text="没有更多了"
               @load="onLoad">
-      <van-cell v-for="(item,index) in list"
+      <van-cell v-for="(item, index) in list"
                 :key="index"
-                :title="item.aut_name">
+                :title="comment.aut_name">
         <van-image slot="icon"
                    round
                    width="30"
                    height="30"
                    style="margin-right: 10px;"
-                   :src="item.aut_photo" />
+                   :src="comment.aut_photo" />
         <span style="color: #466b9d;"
-              slot="title">{{ item.aut_name }}</span>
+              slot="title">{{ comment.aut_name }}</span>
         <div slot="label">
-          <p style="color: #363636;">{{ item.content }}</p>
+          <p style="color: #363636;">{{ comment.content }}</p>
           <p>
-            <span style="margin-right: 10px;">{{ item.pubdate | relativeTime }}</span>
-            <van-button size="mini"
-                        @click="onReplyShow(item)"
-                        type="default">回复 {{ item.reply_count }}</van-button>
+            <span style="margin-right: 10px;">{{ comment.pubdate | relativeTime }}</span>
           </p>
         </div>
-        <!-- 已经点赞为实心  空心-o -->
         <van-icon slot="right-icon"
-                  :name="item.is_liking ? 'like' : 'like-o'"
+                  :name="comment.is_liking ? 'like' : 'like-o'"
                   @click="onCommentLike(item)" />
       </van-cell>
     </van-list>
-    <!-- 评论列表 -->
+    <!-- 回复列表 -->
 
-    <!-- 发布评论 -->
+    <!-- 发布回复 -->
     <van-cell-group class="publish-wrap">
-      <van-field clearable
-                 v-model="commentText"
+      <van-field v-model="commentText"
+                 clearable
                  placeholder="请输入评论内容">
         <van-button slot="button"
                     size="mini"
-                    @click="onAddComment"
-                    type="info">发布</van-button>
+                    type="info"
+                    @click="onAddComment">发布</van-button>
       </van-field>
     </van-cell-group>
-    <!-- /发布评论 -->
-
-    <!-- 评论回复 -->
-    <van-popup v-model="isReplyShow"
-               round
-               position="bottom"
-               :style="{ height: '90%' }">
-      <!-- 弹层组件 -->
-      <comment-reply :comment="currentComment"
-                     v-if="isReplyShow" />
-    </van-popup>
-    <!-- /评论回复 -->
-
+    <!-- /发布回复 -->
   </div>
 </template>
 
@@ -67,23 +82,17 @@ import {
   addComments,
   addCommentsLike,
   unCommentsLike } from '@/api/comment'
-import CommentReply from './comment-reply'
 
 export default {
-  name: 'ArticleComment',
-  components: {
-    CommentReply
-  },
-  props: {},
+  name: 'CommentReply',
+  props: ['comment'],
   data () {
     return {
       list: [], // 评论列表
       loading: false, // 上拉加载更多的 loading
       finished: false, // 是否加载结束
       offset: null, // 默认没有东西
-      commentText: '', // 评论内容
-      isReplyShow: false, // 控制弹层
-      currentComment: {} // 查看回复的当前评论
+      commentText: '' // 评论内容
     }
   },
 
@@ -94,8 +103,8 @@ export default {
     async onLoad () {
       const { data } = await getComments({
         // 1. 请求数据
-        type: 'a', // 评论类型，a-对文章(article)的评论，c-对评论(comment)的回复
-        source: this.$route.params.articleId, // 源id，文章id或评论id
+        type: 'c', // 评论类型，a-对文章(article)的评论，c-对评论(comment)的回复
+        source: this.comment.com_id.toString(), // 源id，文章id或评论id
         offset: this.offset // 获取评论数据的偏移量，值为评论id，表示从此id的数据向后取，不传表示从第一页开始读取数据
         // limit获取的评论数据个数，不传表示采用后端服务设定的默认每页数据量
       })
@@ -163,7 +172,7 @@ export default {
     },
 
     /**
-        * 展示回复弹层
+        * 评论点赞/取消点赞
         */
     onReplyShow (comment) {
       this.currentComment = comment
